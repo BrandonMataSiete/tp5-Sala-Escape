0d583c49b719b140fc42511af695b822dad3d8f7
elPepeSech777 <felipeciroroballo@gmail.com>
2024-08-02 08:36:04 -0300

    subiendo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP_sala_escape_Roballo_De_La_Fuente.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
